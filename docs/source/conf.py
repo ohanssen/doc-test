f6b92b7d5aba95859e9136fbff349e136f336ddb
Øyvind Hanssen (LA7ECA) <ohanssen@acm.org>
2023-01-30 20:23:38 +0100

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DocTEst'
+copyright = '2025, Ohanssen'
+author = 'Ohanssen'
 
 release = '0.1'
 version = '0.1.0'
